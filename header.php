7ba453c18d5e40efc519425f3e61479b6c515f9e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-06 23:14:12 +0800

    cs

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="html">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="">
+    <meta name="format-detection" content="telephone=no">
+    <?php if (!is_404() && !is_search()) { ?>
+        <link rel="canonical" href="<?= canonical() ?>" />
+    <?php } ?>
+    <title>
+        <?php wp_title('') ?>
+    </title>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <main class="main">
