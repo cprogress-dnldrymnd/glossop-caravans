6ddda12c0d61178f1b67a0a76b7b6b2d0e2f56a4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 16:21:33 +0800

    css

--- template-parts/header/search-stock.php
+++ template-parts/header/search-stock.php
@@ -1,4 +1,4 @@
-<div class="seach-stock-holder background-secondary">
+<div class="seach-stock-holder">
     <div class="container">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
@@ -12,125 +12,129 @@
             </li>
 
         </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="Caravans-tab-pane" role="tabpanel" aria-labelledby="Caravans-tab" tabindex="0">
-                <form action="" class="form-holder">
-                    <div class="row align-items-end">
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Type',
-                                'id' => 'Type',
-                                'label' => 'Type',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'New or Used?',
-                                    'New' => 'New',
-                                    'Used' => 'Used',
-                                    'Both' => 'Both',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Berths',
-                                'id' => 'Berths',
-                                'label' => 'Berths',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'How many berths?',
-                                    'All' => 'All',
-                                    '2' => '2',
-                                    '3' => '3',
-                                    '4' => '4',
-                                    '5' => '5',
-                                    '6' => '6',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Make',
-                                'id' => 'Make',
-                                'label' => 'Make',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'Select Make',
-                                    'Option 1' => 'Option 1',
-                                    'Option 2' => 'Option 2',
-                                    'Option 3' => 'Option 3',
-                                    'Option 4' => 'Option 4',
-                                    'Option 5' => 'Option 5',
-                                    'Option 6' => ' Option6',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Model',
-                                'id' => 'Model',
-                                'label' => 'Model',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'Select Model',
-                                    'Option 1' => 'Option 1',
-                                    'Option 2' => 'Option 2',
-                                    'Option 3' => 'Option 3',
-                                    'Option 4' => 'Option 4',
-                                    'Option 5' => 'Option 5',
-                                    'Option 6' => ' Option6',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Price-Min',
-                                'id' => 'Price-Min',
-                                'label' => 'Price(min.)',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'Any',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <?php
-                            form_control(array(
-                                'type' => 'select',
-                                'name' => 'Price-Max',
-                                'id' => 'Price-Max',
-                                'label' => 'Price(max.)',
-                                'class' => 'form-control-lg',
-                                'options' => array(
-                                    '' => 'Any',
-                                ),
-                            ));
-                            ?>
-                        </div>
-                        <div class="col">
-                            <div class="button-box">
-                                <button type="submit" class="btn btn-primary btn-lg">Search</button>
+    </div>
+    <div class="background-secondary">
+        <div class="container">
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="Caravans-tab-pane" role="tabpanel" aria-labelledby="Caravans-tab" tabindex="0">
+                    <form action="" class="form-holder">
+                        <div class="row align-items-end">
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Type',
+                                    'id' => 'Type',
+                                    'label' => 'Type',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'New or Used?',
+                                        'New' => 'New',
+                                        'Used' => 'Used',
+                                        'Both' => 'Both',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Berths',
+                                    'id' => 'Berths',
+                                    'label' => 'Berths',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'How many berths?',
+                                        'All' => 'All',
+                                        '2' => '2',
+                                        '3' => '3',
+                                        '4' => '4',
+                                        '5' => '5',
+                                        '6' => '6',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Make',
+                                    'id' => 'Make',
+                                    'label' => 'Make',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'Select Make',
+                                        'Option 1' => 'Option 1',
+                                        'Option 2' => 'Option 2',
+                                        'Option 3' => 'Option 3',
+                                        'Option 4' => 'Option 4',
+                                        'Option 5' => 'Option 5',
+                                        'Option 6' => ' Option6',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Model',
+                                    'id' => 'Model',
+                                    'label' => 'Model',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'Select Model',
+                                        'Option 1' => 'Option 1',
+                                        'Option 2' => 'Option 2',
+                                        'Option 3' => 'Option 3',
+                                        'Option 4' => 'Option 4',
+                                        'Option 5' => 'Option 5',
+                                        'Option 6' => ' Option6',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Price-Min',
+                                    'id' => 'Price-Min',
+                                    'label' => 'Price(min.)',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'Any',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <?php
+                                form_control(array(
+                                    'type' => 'select',
+                                    'name' => 'Price-Max',
+                                    'id' => 'Price-Max',
+                                    'label' => 'Price(max.)',
+                                    'class' => 'form-control-lg',
+                                    'options' => array(
+                                        '' => 'Any',
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                            <div class="col">
+                                <div class="button-box">
+                                    <button type="submit" class="btn btn-primary btn-lg">Search</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+                <div class="tab-pane fade" id="Motorhomes-tab-pane" role="tabpanel" aria-labelledby="Motorhomes-tab" tabindex="0">...</div>
+                <div class="tab-pane fade" id="Statics-tab-pane" role="tabpanel" aria-labelledby="Statics-tab" tabindex="0">...</div>
             </div>
-            <div class="tab-pane fade" id="Motorhomes-tab-pane" role="tabpanel" aria-labelledby="Motorhomes-tab" tabindex="0">...</div>
-            <div class="tab-pane fade" id="Statics-tab-pane" role="tabpanel" aria-labelledby="Statics-tab" tabindex="0">...</div>
         </div>
     </div>
 </div>
