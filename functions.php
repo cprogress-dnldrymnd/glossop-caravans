c3dcdb5b8961401abceca69110a990c767d8a9e1
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 18:16:00 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ define('vendor_dir', assets_dir . 'vendors/');
 function action_wp_enqueue_scripts()
 {
     wp_enqueue_style('style', theme_dir . 'style.css');
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap', vendor_dir . 'bootstrap/dist/js/bootstrap.min.js');
     wp_enqueue_script('swiper', vendor_dir . 'swiper/js/swiper-bundle.min.js');
 
