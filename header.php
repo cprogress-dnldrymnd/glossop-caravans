67420da88b277cb2f3472461ca8e65871f96018a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 15:26:18 +0800

    Update header.php

--- header.php
+++ header.php
@@ -39,9 +39,6 @@
                             <div class="offcanvas-body">
                                 <nav class="navbar">
                                     <div class="container-fluid">
-                                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                            <span class="navbar-toggler-icon"></span>
-                                        </button>
                                         <?php
                                         wp_nav_menu(array(
                                             'theme_location' => 'header-menu',
