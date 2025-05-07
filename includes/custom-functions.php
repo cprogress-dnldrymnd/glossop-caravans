11b883dc93e4ebf3233759dda7611986d0e3b384
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 15:49:03 +0800

    cs

--- includes/custom-functions.php
+++ includes/custom-functions.php
@@ -7,13 +7,13 @@ function form_control($args)
     if ($args['type'] == 'text') {
         echo '<input type="text" name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '" value="' . $args['value'] . '">';
     } elseif ($args['type'] == 'select') {
-        echo '<select name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control">';
+        echo '<select name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '">';
         foreach ($args['options'] as $key => $value) {
             echo '<option value="' . $key . '">' . $value . '</option>';
         }
         echo '</select>';
     } elseif ($args['type'] == 'textarea') {
-        echo '<textarea name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control" placeholder="' . $args['placeholder'] . '">' . $args['value'] . '</textarea>';
+        echo '<textarea name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '">' . $args['value'] . '</textarea>';
     }
     echo '</div>';
 }
