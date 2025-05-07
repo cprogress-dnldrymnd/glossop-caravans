c5a26a36f35e958a0f5c5de9adfc263632dfa1d5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 17:04:41 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -3,3 +3,36 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Complex_Container;
 use Carbon_Fields\Field;
+use Carbon_Fields\Block;
+
+Block::make(__('Grid'))
+    ->add_fields(array(
+        Field::make('text', 'heading', __('Heading')),
+        Field::make('textarea', 'description', __('Description')),
+        Field::make('complex', 'grid', __('Grid'))
+            ->add_fields(array(
+                Field::make('text', 'title', __('Grid Title')),
+                Field::make('image', 'image', __('Grid Image')),
+                Field::make('textarea', 'description', __('Grid Description')),
+                Field::make('text', 'grid_tag', __('Grid Tag')),
+            ))
+    ))
+    ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
+?>
+
+    <div class="block">
+        <div class="block__heading">
+            <h1><?php echo esc_html($fields['heading']); ?></h1>
+        </div><!-- /.block__heading -->
+
+        <div class="block__image">
+            <?php echo wp_get_attachment_image($fields['image'], 'full'); ?>
+        </div><!-- /.block__image -->
+
+        <div class="block__content">
+            <?php echo apply_filters('the_content', $fields['content']); ?>
+        </div><!-- /.block__content -->
+    </div><!-- /.block -->
+
+<?php
+    });
