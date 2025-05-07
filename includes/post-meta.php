838cbe07ca523510b443dbf350fa334b0dfa0e9a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 17:27:52 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -27,7 +27,7 @@ Block::make(__('Grid Items'))
         <div class="row">
             <?php foreach ($grid as $item) : ?>
                 <div class="col-lg-4">
-                    <div class="grid-inner h-100 d-flex flex-column justif-content-between" style="background-color: <?php echo esc_attr($item['bg_color']); ?>;">
+                    <a href="><?php echo esc_html($item['grid_link']); ?>" class="grid-inner h-100 d-flex flex-column justif-content-between" style="background-color: <?php echo esc_attr($item['bg_color']); ?>;">
                         <div class="grid-item__tag">
                             <div class="grid-item__image">
                                 <?php echo wp_get_attachment_image($item['image'], 'full'); ?>
@@ -38,7 +38,7 @@ Block::make(__('Grid Items'))
                                 <p><?php echo esc_html($item['description']); ?></p>
                             </div><!-- /.grid-item__content -->
                         </div>
-                    </div><!-- /.grid-item -->
+                    </a><!-- /.grid-item -->
                 <?php endforeach; ?>
                 </div>
         </div>
