c5f9b1a9fa61a768b7b00bed829d04abe8b2e24b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 22:15:54 +0800

    Update listing.php

--- templates/listing.php
+++ templates/listing.php
@@ -5,11 +5,17 @@
 ?>
 <?php get_header() ?>
 
-<div class="site-content full-width-content">
-    <?php while (have_posts()) { ?>
-        <?php the_post() ?>
-        <?php the_content() ?>
-    <?php } ?>
+<div class="site-content listings">
+    <div class="container">
+        <div class="listings-holder">
+            <h2>We found <span class="text-orange">59</span> caravans for you</h2>
+            <div class="row">
+                <div class="col-lg-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer() ?>
