4a72d9d928e87ec23bc09b7790df07f20c399d31
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 15:40:40 +0800

    Update search-stock.php

--- template-parts/header/search-stock.php
+++ template-parts/header/search-stock.php
@@ -1,5 +1,21 @@
 <div class="seach-stock-holder">
     <div class="container">
-        sdsdsds
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="Caravans-tab" data-bs-toggle="tab" data-bs-target="#Caravans-tab-pane" type="button" role="tab" aria-controls="Caravans-tab-pane" aria-selected="true">Caravans</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="Motorhomes-tab" data-bs-toggle="tab" data-bs-target="#Motorhomes-tab-pane" type="button" role="tab" aria-controls="Motorhomes-tab-pane" aria-selected="false">Motorhomes</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="Statics-tab" data-bs-toggle="tab" data-bs-target="#Statics-tab-pane" type="button" role="tab" aria-controls="Statics-tab-pane" aria-selected="false">Statics</button>
+            </li>
+         
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="Caravans-tab-pane" role="tabpanel" aria-labelledby="Caravans-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="Motorhomes-tab-pane" role="tabpanel" aria-labelledby="Motorhomes-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="Statics-tab-pane" role="tabpanel" aria-labelledby="Statics-tab" tabindex="0">...</div>
+        </div>
     </div>
 </div>
