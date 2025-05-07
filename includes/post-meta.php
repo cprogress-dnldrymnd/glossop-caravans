0e587db2d73e81fb5eeb9399099e447f467c60d6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 17:27:13 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -9,6 +9,7 @@ Block::make(__('Grid Items'))
     ->add_fields(array(
         Field::make('complex', 'grid', __('Grid Items'))
             ->add_fields(array(
+                Field::make('color', 'bg_color', __('Background Color')),
                 Field::make('text', 'title', __('Grid Title')),
                 Field::make('image', 'image', __('Grid Image')),
                 Field::make('textarea', 'description', __('Grid Description')),
@@ -26,19 +27,21 @@ Block::make(__('Grid Items'))
         <div class="row">
             <?php foreach ($grid as $item) : ?>
                 <div class="col-lg-4">
-                    <div class="grid-inner">
-                        <div class="grid-item__image">
-                            <?php echo wp_get_attachment_image($item['image'], 'full'); ?>
-                            <h3><?php echo esc_html($item['title']); ?></h3>
+                    <div class="grid-inner h-100 d-flex flex-column justif-content-between" style="background-color: <?php echo esc_attr($item['bg_color']); ?>;">
+                        <div class="grid-item__tag">
                             <span class="tag"><?php echo esc_html($item['grid_tag']); ?></span>
-                        </div><!-- /.grid-item__image -->
-                        <div class="grid-item__content">
-                            <p><?php echo esc_html($item['description']); ?></p>
-                        </div><!-- /.grid-item__content -->
-                    </div>
-                </div><!-- /.grid-item -->
-            <?php endforeach; ?>
+                            <div class="grid-item__image">
+                                <?php echo wp_get_attachment_image($item['image'], 'full'); ?>
+                                <h3><?php echo esc_html($item['title']); ?></h3>
+                                <span class="tag"><?php echo esc_html($item['grid_tag']); ?></span>
+                            </div><!-- /.grid-item__image -->
+                            <div class="grid-item__content">
+                                <p><?php echo esc_html($item['description']); ?></p>
+                            </div><!-- /.grid-item__content -->
+                        </div>
+                    </div><!-- /.grid-item -->
+                <?php endforeach; ?>
+                </div>
         </div>
-    </div>
-<?php
+    <?php
     });
