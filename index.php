fded0ed89ddb703d639b70e524d86e026d770406
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 14:39:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php get_header() ?>
+<main id="primary" class="site-main">
 
-<?php get_footer() ?>
+    <?php
+    if (have_posts()) :
+
+        if (is_home() && ! is_front_page()) :
+    ?>
+    <?php
+        endif;
+
+        /* Start the Loop */
+        while (have_posts()) :
+            the_post();
+
+            /**
+             * Include the Post-Type-specific template for the content.
+             * If you want to overload this in a child theme then include a file
+             * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+             */
+            get_template_part('template-parts/content', get_post_type());
+
+        endwhile;
+
+        the_posts_navigation();
+
+    else :
+
+        get_template_part('template-parts/content', 'none');
+
+    endif;
+    ?>
+    <?php get_footer() ?>
