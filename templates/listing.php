79eb98a0bcd63a53b5a2e5dc25f0dafce4e86093
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 22:47:15 +0800

    css

--- templates/listing.php
+++ templates/listing.php
@@ -10,7 +10,7 @@ global $listing_fields;
 <div class="site-content listings background-lightgray">
     <div class="container md-padding-top md-padding-bottom">
         <div class="listings-holder">
-            <h2>We found <span class="text-orange">59</span> caravans for you</h2>
+            <h2 class="mb-5">We found <span class="text-orange">59</span> caravans for you</h2>
             <div class="row">
                 <div class="col-lg-3">
                     <div class="listing-filter">
