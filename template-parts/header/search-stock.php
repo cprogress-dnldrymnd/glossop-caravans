a80b5ffc6bc683cfbfd735e4e0f7d2605a81da26
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 15:50:14 +0800

    Update search-stock.php

--- template-parts/header/search-stock.php
+++ template-parts/header/search-stock.php
@@ -1,4 +1,4 @@
-<div class="seach-stock-holder background-accent">
+<div class="seach-stock-holder background-secondary">
     <div class="container">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
