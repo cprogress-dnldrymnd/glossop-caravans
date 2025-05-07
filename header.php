e42b27168ad662b8f7a47d803bae8aebc9c4fb92
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-07 15:17:05 +0800

    cs

--- header.php
+++ header.php
@@ -40,21 +40,26 @@
                                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div class="offcanvas-body">
-                                <div class="dropdown mt-3">
-                                    <div id="main-menu">
-                                        <?php
-                                        wp_nav_menu(array(
-                                            'theme_location' => 'header-menu',
-                                            'container' => false,
-                                            'menu_class' => '',
-                                            'fallback_cb' => '__return_false',
-                                            'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-                                            'depth' => 2,
-                                            'walker' => new bootstrap_5_wp_nav_menu_walker()
-                                        ));
-                                        ?>
+                                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                                    <div class="container-fluid">
+                                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                            <span class="navbar-toggler-icon"></span>
+                                        </button>
+                                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                            <?php
+                                            wp_nav_menu(array(
+                                                'theme_location' => 'header-menu',
+                                                'container' => false,
+                                                'menu_class' => '',
+                                                'fallback_cb' => '__return_false',
+                                                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                                                'depth' => 2,
+                                                'walker' => new bootstrap_5_wp_nav_menu_walker()
+                                            ));
+                                            ?>
+                                        </div>
                                     </div>
-                                </div>
+                                </nav>
                             </div>
                         </div>
                     </div>
