3cfafb52f19be229487ff354731ee4761e86e5ee
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 01:55:27 +0800

    Create listing-inner.php

---
+++ templates/listing-inner.php
@@ -0,0 +1,27 @@
+<?php
+/*-----------------------------------------------------------------------------------*/
+/* Template Name: Listing Inner Page 
+/*-----------------------------------------------------------------------------------*/
+?>
+<?php get_header() ?>
+<?php
+global $listing_fields;
+?>
+<div class="site-content listing-inner">
+  <div class="container md-padding-top md-padding-bottom">
+    <div class="listing-inner-holder">
+      <div class="col-lg-9">
+        <div class="listing-inner--left">
+
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="listing-inner--right">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer() ?>
