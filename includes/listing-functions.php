4efb828878c2698e3cda7c53321186ae97172138
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 00:11:22 +0800

    cs

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -5,13 +5,13 @@ function listings_fields()
     global $listing_fields;
 
     $listing_fields['sortby'] = form_control(array(
-        'type' => 'select',
-        'name' => 'Type',
-        'id' => 'Type',
-        'label' => 'Type',
-        'class' => 'form-control-lg',
+        'type'    => 'select',
+        'name'    => 'Type',
+        'id'      => 'Type',
+        'label'   => 'Type',
+        'class'   => 'form-control-lg',
         'options' => array(
-            '' => 'Sort 1',
+            ''         => 'Sort 1',
             'Option 1' => 'Option 1',
             'Option 2' => 'Option 2',
             'Option 3' => 'Option 3',
@@ -21,43 +21,43 @@ function listings_fields()
         ),
     ));
     $listing_fields['type'] = form_control(array(
-        'type' => 'select',
-        'name' => 'Type',
-        'id' => 'Type',
-        'label' => 'Type',
-        'class' => 'form-control-lg',
+        'type'    => 'select',
+        'name'    => 'Type',
+        'id'      => 'Type',
+        'label'   => 'Type',
+        'class'   => 'form-control-lg',
         'options' => array(
-            '' => 'New or Used?',
-            'New' => 'New',
+            ''     => 'New or Used?',
+            'New'  => 'New',
             'Used' => 'Used',
             'Both' => 'Both',
         ),
     ));
 
-    $listing_fields['berths'] =  form_control(array(
-        'type' => 'select',
-        'name' => 'Berths',
-        'id' => 'Berths',
-        'label' => 'Berths',
-        'class' => 'form-control-lg',
+    $listing_fields['berths'] = form_control(array(
+        'type'    => 'select',
+        'name'    => 'Berths',
+        'id'      => 'Berths',
+        'label'   => 'Berths',
+        'class'   => 'form-control-lg',
         'options' => array(
-            '' => 'How many berths?',
+            ''    => 'How many berths?',
             'All' => 'All',
-            '2' => '2',
-            '3' => '3',
-            '4' => '4',
-            '5' => '5',
-            '6' => '6',
+            '2'   => '2',
+            '3'   => '3',
+            '4'   => '4',
+            '5'   => '5',
+            '6'   => '6',
         ),
     ));
     $listing_fields['make'] = form_control(array(
-        'type' => 'select',
-        'name' => 'Make',
-        'id' => 'Make',
-        'label' => 'Make',
-        'class' => 'form-control-lg',
+        'type'    => 'select',
+        'name'    => 'Make',
+        'id'      => 'Make',
+        'label'   => 'Make',
+        'class'   => 'form-control-lg',
         'options' => array(
-            '' => 'Select Make',
+            ''         => 'Select Make',
             'Option 1' => 'Option 1',
             'Option 2' => 'Option 2',
             'Option 3' => 'Option 3',
@@ -66,14 +66,14 @@ function listings_fields()
             'Option 6' => ' Option6',
         ),
     ));
-    $listing_fields['model'] =  form_control(array(
-        'type' => 'select',
-        'name' => 'Model',
-        'id' => 'Model',
-        'label' => 'Model',
-        'class' => 'form-control-lg',
+    $listing_fields['model'] = form_control(array(
+        'type'    => 'select',
+        'name'    => 'Model',
+        'id'      => 'Model',
+        'label'   => 'Model',
+        'class'   => 'form-control-lg',
         'options' => array(
-            '' => 'Select Model',
+            ''         => 'Select Model',
             'Option 1' => 'Option 1',
             'Option 2' => 'Option 2',
             'Option 3' => 'Option 3',
@@ -83,24 +83,68 @@ function listings_fields()
         ),
     ));
     $listing_fields['price_min'] = form_control(array(
-        'type' => 'select',
-        'name' => 'Price-Min',
-        'id' => 'Price-Min',
-        'label' => 'Price(min.)',
-        'class' => 'form-control-lg',
+        'type'    => 'select',
+        'name'    => 'Price-Min',
+        'id'      => 'Price-Min',
+        'label'   => 'Price(min.)',
+        'class'   => 'form-control-lg',
         'options' => array(
             '' => 'Any',
         ),
     ));
     $listing_fields['price_max'] = form_control(array(
-        'type' => 'select',
-        'name' => 'Price-Max',
-        'id' => 'Price-Max',
-        'label' => 'Price(max.)',
-        'class' => 'form-control-lg',
+        'type'    => 'select',
+        'name'    => 'Price-Max',
+        'id'      => 'Price-Max',
+        'label'   => 'Price(max.)',
+        'class'   => 'form-control-lg',
         'options' => array(
             '' => 'Any',
         ),
     ));
 }
 add_action('after_setup_theme', 'listings_fields');
+
+
+
+function listing__price()
+{
+    ob_start();
+    ?>
+    <div class="listing-grid-item__prices-holder">
+        <div class="listing-grid-item__prices row g-xxs text-center">
+            <div class="listing-grid-item__price col-md-4">
+                <div class="grid-item__price-inner rounded h-100">
+                    <span class="fs-14">RRP</span>
+                    <strong><s>£44,125</s></strong>
+                </div>
+            </div>
+            <div class="listing-grid-item__price col-md-4">
+                <div class="grid-item__price-inner rounded h-100">
+                    <span class="fs-14">Our price</span>
+                    <strong>£42,200</strong>
+                </div>
+            </div>
+            <div class="listing-grid-item__price col-md-4">
+                <div class="grid-item__price-inner rounded h-100">
+                    <span class="fs-14">Save</span>
+                    <strong class="text-orange-2">£1,955</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
+function listing__action()
+{
+    ob_start();
+    ?>
+    <ul class="icon-list d-flex list-inline align-items-center">
+        <li> <?= get__theme_images('share.svg') ?> Share</li>
+        <li><?= get__theme_images(file_name: 'save.svg') ?> Save</li>
+    </ul>
+    <?php
+    return ob_get_clean();
+}
