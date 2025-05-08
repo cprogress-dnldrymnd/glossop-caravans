8251704c18c4fff777f3275be6d658c48c0f4826
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 03:16:18 +0800

    css

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -186,7 +186,7 @@ function listing__gallery($id, $is_thumbnail = false)
                 <div class="swiper-slide <?= $is_thumbnail == false ? '' : '' ?>">
                     <a href="<?= wp_get_attachment_image_url(53, 'full') ?>" data-fancybox="<?= $id ?>"
                         class="d-block image-box image-style <?= $is_thumbnail == false ? '' : '' ?>">
-                        <?= wp_get_attachment_image(53, is_thumbnail == false ? 'large' : 'medium') ?>
+                        <?= wp_get_attachment_image(53, $is_thumbnail == false ? 'large' : 'medium') ?>
                     </a>
                 </div>
             </div>
