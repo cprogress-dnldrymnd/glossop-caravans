3aff7b646dce79362503948424c0cd3a3b9621de
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-08 22:36:36 +0800

    cs

--- includes/custom-functions.php
+++ includes/custom-functions.php
@@ -1,21 +1,23 @@
 <?php
 function form_control($args)
 {
-    $html =  '<div class="form-control-holder">';
-    $html .=  '<label for="' . $args['id'] . '">' . $args['label'] . '</label>';
+    $html = '<div class="form-control-holder">';
+    $html .= '<label for="' . $args['id'] . '">' . $args['label'] . '</label>';
 
     if ($args['type'] == 'text') {
-        $html .=  '<input type="text" name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '" value="' . $args['value'] . '">';
-    } elseif ($args['type'] == 'select') {
-        $html .=  '<select name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '">';
-        foreach ($args['options'] as $key => $value) {
-            $html .=  '<option value="' . $key . '">' . $value . '</option>';
-        }
-        $html .=  '</select>';
-    } elseif ($args['type'] == 'textarea') {
-        $html .=  '<textarea name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '">' . $args['value'] . '</textarea>';
+        $html .= '<input type="text" name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '" value="' . $args['value'] . '">';
     }
-    $html .=  '</div>';
+    elseif ($args['type'] == 'select') {
+        $html .= '<select name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '">';
+        foreach ($args['options'] as $key => $value) {
+            $html .= '<option value="' . $key . '">' . $value . '</option>';
+        }
+        $html .= '</select>';
+    }
+    elseif ($args['type'] == 'textarea') {
+        $html .= '<textarea name="' . $args['name'] . '" id="' . $args['id'] . '" class="form-control ' . $args['class'] . '" placeholder="' . $args['placeholder'] . '">' . $args['value'] . '</textarea>';
+    }
+    $html .= '</div>';
 
     return $html;
 }
@@ -25,7 +27,38 @@ function get__theme_images($file_name, $image_tag = true)
 {
     if ($image_tag) {
         return '<img src="' . image_dir . $file_name . '" alt="' . $file_name . '">';
-    } else {
-        return  image_dir . $file_name;
     }
+    else {
+        return image_dir . $file_name;
+    }
+}
+
+
+function listing__price()
+{
+    ob_start();
+    ?>
+    <div class="listing-grid-item__prices row g-xxs text-center">
+        <div class="listing-grid-item__price col-md-4">
+            <div class="grid-item__price-inner rounded h-100">
+                <span class="fs-14">RRP</span>
+                <strong><s>£44,125</s></strong>
+            </div>
+        </div>
+        <div class="listing-grid-item__price col-md-4">
+            <div class="grid-item__price-inner rounded h-100">
+                <span class="fs-14">Our price</span>
+                <strong>£42,200</strong>
+            </div>
+        </div>
+        <div class="listing-grid-item__price col-md-4">
+            <div class="grid-item__price-inner rounded h-100">
+                <span class="fs-14">Save</span>
+                <strong class="text-orange-2">£1,955</strong>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    return ob_get_clean();
 }
--- template-parts/shortcodes/listing-grid-full-details.php
+++ template-parts/shortcodes/listing-grid-full-details.php
@@ -2,7 +2,7 @@
   <div class="row g-0">
     <div class="col-lg-9">
       <div class="listing-grid--left-inner">
-        <?= $listing__price ?>
+        <?= listing__price() ?>
       </div>
     </div>
     <div class="col-lg-3">
