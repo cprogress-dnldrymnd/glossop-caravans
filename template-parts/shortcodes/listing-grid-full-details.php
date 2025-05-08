8b47521052c38ef4d791bf553f3970c2239baeb5
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-08 22:40:37 +0800

    Update listing-grid-full-details.php

--- template-parts/shortcodes/listing-grid-full-details.php
+++ template-parts/shortcodes/listing-grid-full-details.php
@@ -1,11 +1,11 @@
 <div class="listing-grid-full-details bg-white rounded-overflow-hidden">
   <div class="row g-0">
-    <div class="col-lg-9">
+    <div class="col-lg-8">
       <div class="listing-grid--left-inner">
         <?= listing__price() ?>
       </div>
     </div>
-    <div class="col-lg-3">
+    <div class="col-lg-4">
       <div class="listing-grid--right-inner">
         <div class="image-box">
           <?= wp_get_attachment_image(189, 'large') ?>
