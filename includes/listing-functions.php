1be19f5293b61678091a3aab2357971a2e08c1d3
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 00:30:47 +0800

    css

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -185,7 +185,7 @@ function listing__icons()
 {
     ob_start();
     ?>
-    <ul class="icon-list d-flex list-inline align-items-center justify-content-end fw-semibold">
+    <ul class="icon-list icon-list-v2 d-flex list-inline align-items-center justify-content-end fw-semibold flex-wrap">
         <li> <?= get__theme_images('berths.svg') ?> 6 Berth</li>
         <li><?= get__theme_images('warranty.svg') ?> 3 year warranty</li>
         <li><?= get__theme_images('year.svg') ?> Year 2024</li>
