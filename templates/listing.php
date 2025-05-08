748faf7f88d2d7b5bfca1d7c5300aab26e51bb31
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-08 22:11:52 +0800

    Update listing.php

--- templates/listing.php
+++ templates/listing.php
@@ -17,12 +17,18 @@ global $listing_fields;
                         <div class="accordion rounded" id="accordionFilter">
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSort-by" aria-expanded="false" aria-controls="collapseSort-by">
-                                        <span class="icon"><?= get__theme_images('sort-by.svg') ?></span> Sort by
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseSort-by" aria-expanded="false"
+                                        aria-controls="collapseSort-by">
+                                        <span class="icon-text">
+                                            <span class="icon"><?= get__theme_images('sort-by.svg') ?></span>
+                                            Sort by
+                                        </span>
                                         <span class="selected fs-14 fw-bold">Price: low to high</span>
                                     </button>
                                 </h2>
-                                <div id="collapseSort-by" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseSort-by" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['sortby'] ?>
                                     </div>
@@ -30,11 +36,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNew-Used" aria-expanded="false" aria-controls="collapseNew-Used">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseNew-Used" aria-expanded="false"
+                                        aria-controls="collapseNew-Used">
                                         <span class="icon"><?= get__theme_images('new-used.svg') ?></span> New-Used
                                     </button>
                                 </h2>
-                                <div id="collapseNew-Used" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseNew-Used" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
@@ -42,11 +51,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBerths" aria-expanded="false" aria-controls="collapseBerths">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseBerths" aria-expanded="false"
+                                        aria-controls="collapseBerths">
                                         <span class="icon"><?= get__theme_images('berths.svg') ?></span> Berths
                                     </button>
                                 </h2>
-                                <div id="collapseBerths" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseBerths" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['berths'] ?>
                                     </div>
@@ -54,11 +66,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMake" aria-expanded="false" aria-controls="collapseMake">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseMake" aria-expanded="false"
+                                        aria-controls="collapseMake">
                                         <span class="icon"><?= get__theme_images('make.svg') ?></span> Make
                                     </button>
                                 </h2>
-                                <div id="collapseMake" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseMake" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['make'] ?>
                                     </div>
@@ -66,11 +81,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseModel" aria-expanded="false" aria-controls="collapseModel">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseModel" aria-expanded="false"
+                                        aria-controls="collapseModel">
                                         <span class="icon"><?= get__theme_images('model.svg') ?></span> Model
                                     </button>
                                 </h2>
-                                <div id="collapseModel" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseModel" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['model'] ?>
                                     </div>
@@ -78,11 +96,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePrice" aria-expanded="false" aria-controls="collapsePrice">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapsePrice" aria-expanded="false"
+                                        aria-controls="collapsePrice">
                                         <span class="icon"><?= get__theme_images('price.svg') ?></span> Price
                                     </button>
                                 </h2>
-                                <div id="collapsePrice" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapsePrice" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
@@ -90,11 +111,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseYear" aria-expanded="false" aria-controls="collapseYear">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseYear" aria-expanded="false"
+                                        aria-controls="collapseYear">
                                         <span class="icon"><?= get__theme_images('year.svg') ?></span> Year
                                     </button>
                                 </h2>
-                                <div id="collapseYear" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseYear" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
@@ -102,11 +126,15 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLayout-type" aria-expanded="false" aria-controls="collapseLayout-type">
-                                        <span class="icon"><?= get__theme_images('layout-type.svg') ?></span> Layout-type
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseLayout-type" aria-expanded="false"
+                                        aria-controls="collapseLayout-type">
+                                        <span class="icon"><?= get__theme_images('layout-type.svg') ?></span>
+                                        Layout-type
                                     </button>
                                 </h2>
-                                <div id="collapseLayout-type" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseLayout-type" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
@@ -114,11 +142,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidth" aria-expanded="false" aria-controls="collapseWidth">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseWidth" aria-expanded="false"
+                                        aria-controls="collapseWidth">
                                         <span class="icon"><?= get__theme_images('width.svg') ?></span> Width
                                     </button>
                                 </h2>
-                                <div id="collapseWidth" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseWidth" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
@@ -126,11 +157,14 @@ global $listing_fields;
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAxles" aria-expanded="false" aria-controls="collapseAxles">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseAxles" aria-expanded="false"
+                                        aria-controls="collapseAxles">
                                         <span class="icon"><?= get__theme_images('axles.svg') ?></span> Axles
                                     </button>
                                 </h2>
-                                <div id="collapseAxles" class="accordion-collapse collapse" data-bs-parent="#accordionFilter">
+                                <div id="collapseAxles" class="accordion-collapse collapse"
+                                    data-bs-parent="#accordionFilter">
                                     <div class="accordion-body">
                                         <?= $listing_fields['type'] ?>
                                     </div>
