b66cf47592226b76ce648c3c13701c2944ecf9a7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 01:57:57 +0800

    Update listing-inner.php

--- templates/listing-inner.php
+++ templates/listing-inner.php
@@ -10,25 +10,27 @@ global $listing_fields;
 <div class="site-content listing-inner">
   <div class="container md-padding-top md-padding-bottom">
     <div class="listing-inner-holder">
-      <div class="col-lg-9">
-        <div class="listing-inner--left">
-          <div class="listing-inner--top">
-            <div class="row g-3 justify-content-between">
-              <div class="col-md-6">
-                <div class="image-box brand">
-                  <?= wp_get_attachment_image(190, 'medium') ?>
+      <div class="row g-4">
+        <div class="col-lg-9">
+          <div class="listing-inner--left">
+            <div class="listing-inner--top">
+              <div class="row g-3 justify-content-between">
+                <div class="col-md-6">
+                  <div class="image-box brand">
+                    <?= wp_get_attachment_image(190, 'medium') ?>
+                  </div>
                 </div>
-              </div>
-              <div class="col-md-6">
-                <?= listing__action() ?>
+                <div class="col-md-6">
+                  <?= listing__action() ?>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="listing-inner--right">
+        <div class="col-lg-3">
+          <div class="listing-inner--right">
 
+          </div>
         </div>
       </div>
     </div>
