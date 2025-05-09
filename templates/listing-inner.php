75b3f153197d75b3f922526c391513aabee21be9
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-09 14:04:14 +0800

    Update listing-inner.php

--- templates/listing-inner.php
+++ templates/listing-inner.php
@@ -223,7 +223,7 @@ global $listing_fields;
             <div class="cta finance-calculator">
               <div class="cta--iner p-20 rounded box-shadow">
                 <h3 class="fs-32">Finance calculator</h3>
-                <div class="desc">
+                <div class="desc mb-20">
                   <p>7.9% available, calculate the cost of your caravan or motorhome</p>
                 </div>
                 <a class="btn btn-primary w-100">Search</a>
