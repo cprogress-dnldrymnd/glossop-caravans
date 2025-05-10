5cb23c6ef6fb247680b881045ffa91cf15ab64e4
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-10 18:45:22 +0800

    css

--- template-parts/offcanvas/reserve-form.php
+++ template-parts/offcanvas/reserve-form.php
@@ -8,14 +8,18 @@
           <div class="offcanvas-form--form-header background-gray p-20">
             <h4 class="fs-32">Reserve this caravan for free</h4>
           </div>
-          <div class="desc fs-12 mb-20">
-            <p> Fill in the form below to reserve the <span class="name"><strong>Elddis Avante 462 2012</strong></span>
-              and a member of the team will be in
-              contact with you to arrange a viewing.</p>
-          </div>
+
           <div class="offcanvas-form--form-fields">
             <form action="" class="offcanvas-form mb-20">
               <div class="row g-3">
+                <div class="col-lg-12">
+                  <div class="desc">
+                    <p> Fill in the form below to reserve the <span class="name"><strong>Elddis Avante 462
+                          2012</strong></span>
+                      and a member of the team will be in
+                      contact with you to arrange a viewing.</p>
+                  </div>
+                </div>
                 <div class="col-lg-6">
                   <?php
                   echo form_control(array(
@@ -134,7 +138,7 @@
                   ));
                   ?>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                   <?php
                   echo form_control(array(
                     'type'  => 'textarea',
