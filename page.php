09fddf877c0b6027ca6126adfdbbbfbdd0a0e45e
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-10 18:53:50 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
-<div class="site-content site-content--default">
+<div class="site-content site-content--default md-padding-top md-padding-bottom background-light-gray-4">
     <div class="container">
         <?php while (have_posts()) { ?>
             <h1 class="fs-35 sm-margin-bottom"><?php the_title() ?></h1>
-            <div class="site-content--inner background-white p-4 rounded overflow-hidden">
+            <div class="site-content--inner background-white p-4 rounded overflow-hiddens">
                 <?php the_post() ?>
                 <?php the_content() ?>
             </div>
