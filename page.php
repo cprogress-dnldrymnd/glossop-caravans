b8c04e3950fa577c4491db313ede2362bc978054
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-10 18:53:10 +0800

    Update page.php

--- page.php
+++ page.php
@@ -3,8 +3,8 @@
 <div class="site-content site-content--default">
     <div class="container">
         <?php while (have_posts()) { ?>
-            <h1><?php the_title() ?></h1>
-            <div class="site-content--inner">
+            <h1 class="fs-35 sm-margin-bottom"><?php the_title() ?></h1>
+            <div class="site-content--inner background-white p-4 rounded overflow-hidden">
                 <?php the_post() ?>
                 <?php the_content() ?>
             </div>
