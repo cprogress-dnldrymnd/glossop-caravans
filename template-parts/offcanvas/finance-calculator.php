4150c1707fd776ba74074fdc24c863f53112c2bf
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-05-10 17:51:52 +0800

    Update finance-calculator.php

--- template-parts/offcanvas/finance-calculator.php
+++ template-parts/offcanvas/finance-calculator.php
@@ -1,4 +1,4 @@
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasFinanceCalculator" aria-labelledby="offcanvasFinanceCalculatorLabel">
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasFinanceCalculator" aria-labelledby="offcanvasFinanceCalculatorLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="offcanvasFinanceCalculatorLabel">Offcanvas</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
