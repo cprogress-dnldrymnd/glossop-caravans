2fd079552e85e09ad87572ffb2f96624477e01f3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-12 21:57:19 +0800

    css

--- template-parts/shortcodes/latest-deals.php
+++ template-parts/shortcodes/latest-deals.php
@@ -1,20 +1,22 @@
 <div class="deals-swiper-holder">
     <div class="tab-nav-holder mb-5">
         <div class="container">
-            <ul class="nav nav-tabs nav-tabs-style-2" id="myTabDeals-Swiper" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="Weekly-Deals-tab" data-bs-toggle="tab" data-bs-target="#Weekly-Deals-tab-pane" type="button" role="tab" aria-controls="Weekly-Deals-tab-pane" aria-selected="true">Weekly Deals</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="New-Caravan-Offers-tab" data-bs-toggle="tab" data-bs-target="#New-Caravan-Offers-tab-pane" type="button" role="tab" aria-controls="New-Caravan-Offers-tab-pane" aria-selected="false">New Caravan Offers</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="New-Motorhome-Offers-tab" data-bs-toggle="tab" data-bs-target="#New-Motorhome-Offers-tab-pane" type="button" role="tab" aria-controls="New-Motorhome-Offers-tab-pane" aria-selected="false">New Motorhome Offers</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link text-red" id="Clearance-tab" data-bs-toggle="tab" data-bs-target="#Clearance-tab-pane" type="button" role="tab" aria-controls="Clearance-tab-pane" aria-selected="false">Clearance</button>
-                </li>
-            </ul>
+            <div class="nav-tabs-swiper nav-tabs-swiper swiper">
+                <ul class="swiper-wrapper nav nav-tabs nav-tabs-style-2" id="myTabDeals-Swiper" role="tablist">
+                    <li class="swiper-slide nav-item" role="presentation">
+                        <button class="nav-link active" id="Weekly-Deals-tab" data-bs-toggle="tab" data-bs-target="#Weekly-Deals-tab-pane" type="button" role="tab" aria-controls="Weekly-Deals-tab-pane" aria-selected="true">Weekly Deals</button>
+                    </li>
+                    <li class="swiper-slide nav-item" role="presentation">
+                        <button class="nav-link" id="New-Caravan-Offers-tab" data-bs-toggle="tab" data-bs-target="#New-Caravan-Offers-tab-pane" type="button" role="tab" aria-controls="New-Caravan-Offers-tab-pane" aria-selected="false">New Caravan Offers</button>
+                    </li>
+                    <li class="swiper-slide nav-item" role="presentation">
+                        <button class="nav-link" id="New-Motorhome-Offers-tab" data-bs-toggle="tab" data-bs-target="#New-Motorhome-Offers-tab-pane" type="button" role="tab" aria-controls="New-Motorhome-Offers-tab-pane" aria-selected="false">New Motorhome Offers</button>
+                    </li>
+                    <li class="swiper-slide nav-item" role="presentation">
+                        <button class="nav-link text-red" id="Clearance-tab" data-bs-toggle="tab" data-bs-target="#Clearance-tab-pane" type="button" role="tab" aria-controls="Clearance-tab-pane" aria-selected="false">Clearance</button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="tab-cotnent-holder ">
