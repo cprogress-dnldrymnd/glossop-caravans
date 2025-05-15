0229e45a9d549e36435370d72bf5b3de16e65f37
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-15 16:35:30 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -186,26 +186,28 @@ function listing__gallery($id, $is_thumbnail = false)
         <div class="zoom d-none d-lg-flex">
             <?= get__theme_images('zoom.svg') ?>
         </div>
-        <div class="listing-grid--feature--action  listing-grid--feature--action--style-2 d-flex d-lg-none">
-            <div class="listing-grid__feature fs-13 row g-xxs fw-semibold">
-                <div class="listing-grid__feature-item col-auto">
-                    <div
-                        class="grid__feature-inner rounded h-100 d-flex align-items-center justify-content-center text-center">
-                        Finance available: 7.9% APR
+        <?php if (is_page(186)) { ?>
+            <div class="listing-grid--feature--action  listing-grid--feature--action--style-2 d-flex d-lg-none">
+                <div class="listing-grid__feature fs-13 row g-xxs fw-semibold">
+                    <div class="listing-grid__feature-item col-auto">
+                        <div
+                            class="grid__feature-inner rounded h-100 d-flex align-items-center justify-content-center text-center">
+                            Finance available: 7.9% APR
+                        </div>
+                    </div>
+                    <div class="listing-grid__feature-item col-auto">
+                        <div
+                            class="grid__feature-inner rounded h-100 d-flex flex-column align-items-center justify-content-center text-center">
+                            <span class="fs-7 fw-medium">Per month</span>
+                            £565.50
+                        </div>
                     </div>
                 </div>
-                <div class="listing-grid__feature-item col-auto">
-                    <div
-                        class="grid__feature-inner rounded h-100 d-flex flex-column align-items-center justify-content-center text-center">
-                        <span class="fs-7 fw-medium">Per month</span>
-                        £565.50
-                    </div>
-                </div>
+                <?php
+                echo listing__action(false);
+                ?>
             </div>
-            <?php
-            echo listing__action(false);
-            ?>
-        </div>
+        <?php } ?>
         <div class="swiper <?= $is_thumbnail == false ? 'swiper-gallery h-100' : 'swiper-thumbnails' ?>">
             <div class="swiper-wrapper <?= $is_thumbnail == false ? 'h-100' : '' ?>">
                 <div class="swiper-slide <?= $is_thumbnail == false ? 'h-100' : '' ?>">
