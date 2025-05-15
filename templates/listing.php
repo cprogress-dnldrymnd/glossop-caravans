d7aa8a4625008379ba1a83884158b1e2d30d466f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-15 16:27:35 +0800

    cs

--- templates/listing.php
+++ templates/listing.php
@@ -16,14 +16,14 @@ global $listing_fields;
                 <div class="row g-xxs">
                     <div class="col-6">
                         <button class="btn btn-primary w-100" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasFilter" aria-controls="offCanvasFilter">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
-                                <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                            </svg>
+                            <?= get__theme_images('filter.svg') ?>
+
                             Filter
                         </button>
                     </div>
                     <div class="col-6">
                         <button class="btn btn-light w-100 border">
+                            <?= get__theme_images('sort-by.svg') ?>
                             Sort
                         </button>
                     </div>
