9b052684ca5ed96f7d717bc95ad8a4c162ce03b4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-26 22:20:05 +0800

    Update _header_footer.php

--- _header_footer.php
+++ _header_footer.php
@@ -1,6 +1,11 @@
 <?php
 require_once("./wp-load.php");
+wp_head();
+echo do_shortcode('[contact-form-7 id="373aeba" title="Static Pages Enquiry Form"]');
+wp_footer();
+/*
 global $is_static_page;
 $is_static_page = true;
 get_header('static-pages');
 get_footer('static-pages');
+*/
