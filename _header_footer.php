d1ee3d08dcb97966346757129ce781c60a5899d9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-26 22:02:37 +0800

    cs

--- _header_footer.php
+++ _header_footer.php
@@ -1,4 +1,6 @@
 <?php
 require_once("./wp-load.php");
+global $is_static_page;
+$is_static_page = true;
 get_header('static-pages');
 get_footer('static-pages');
--- wp-content/themes/glossop-caravans/functions.php
+++ wp-content/themes/glossop-caravans/functions.php
@@ -14,20 +14,25 @@ define('vendor_dir', assets_dir . 'vendors/');
 
 function action_after_setup_theme()
 {
+    global $is_static_page;
+    $is_static_page = false;
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'action_after_setup_theme');
 
 function action_wp_enqueue_scripts()
 {
-    wp_enqueue_style('fancybox', vendor_dir . 'fancybox/css/fancybox.css');
-    wp_enqueue_style('style', theme_dir . 'style.css');
+    global $is_static_page;
+    if ($is_static_page == false) {
+        wp_enqueue_style('fancybox', vendor_dir . 'fancybox/css/fancybox.css');
+        wp_enqueue_style('style', theme_dir . 'style.css');
 
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap', vendor_dir . 'bootstrap/dist/js/bootstrap.min.js');
-    wp_enqueue_script('swiper', vendor_dir . 'swiper/js/swiper-bundle.min.js');
-    wp_enqueue_script('fancybox', vendor_dir . 'fancybox/js/fancybox.umd.js');
-    wp_enqueue_script('main', assets_dir . 'javascripts/main.js');
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('bootstrap', vendor_dir . 'bootstrap/dist/js/bootstrap.min.js');
+        wp_enqueue_script('swiper', vendor_dir . 'swiper/js/swiper-bundle.min.js');
+        wp_enqueue_script('fancybox', vendor_dir . 'fancybox/js/fancybox.umd.js');
+        wp_enqueue_script('main', assets_dir . 'javascripts/main.js');
+    }
 }
 add_action('wp_enqueue_scripts', 'action_wp_enqueue_scripts', 20);
 
