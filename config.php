a72df0ae1a839a76666cdd28fb80f0b1dae65b27
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-01 21:14:35 +0800

    deleted html files

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//define('DOMAIN',             'https://glossop10.theprogressteam.co.uk');
-//define("DEV_PREFIX",        'cf-dev/');
-        
-//LIVE SETUP
-define('DOMAIN',             'https://www.glossopcaravans.co.uk');
-define("DEV_PREFIX",        '');
