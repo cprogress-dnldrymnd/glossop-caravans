a9631df6de81b6385c66d8876e8b0ced19ee76ea
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-02 19:56:24 +0800

    cs

--- wp-content/themes/glossop-caravans/single-template.php
+++ wp-content/themes/glossop-caravans/single-template.php
@@ -1,9 +1,10 @@
 <?php
-if (isset($_GET['static_template']) && $_GET['static_template'] == 'true') {
+if (isset($_GET['static_template']) && $_GET['static_template'] != 'true') {
     get_header();
 }
+
 the_content();
 
-if (isset($_GET['static_template']) && $_GET['static_template'] == 'true') {
+if (isset($_GET['static_template']) && $_GET['static_template'] != 'true') {
     get_footer();
 }
