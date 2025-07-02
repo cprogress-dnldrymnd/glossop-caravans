f312abcfb45085b3523607917de0d2c664b2cb61
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-02 19:48:19 +0800

    Update single-template.php

--- wp-content/themes/glossop-caravans/single-template.php
+++ wp-content/themes/glossop-caravans/single-template.php
@@ -1,1 +1,9 @@
-<?php the_content();
+<?php
+if (isset($_GET['static_template']) && $_GET['static_template'] == 'true') {
+    get_header();
+}
+the_content();
+
+if (isset($_GET['static_template']) && $_GET['static_template'] == 'true') {
+    get_footer();
+}
