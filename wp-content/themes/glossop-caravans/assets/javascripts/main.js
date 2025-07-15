9ef3f2cb13612025590431de5f7576d10f551a8c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-15 19:24:58 +0800

    cs

--- wp-content/themes/glossop-caravans/assets/javascripts/main.js
+++ wp-content/themes/glossop-caravans/assets/javascripts/main.js
@@ -209,7 +209,7 @@ function swiper_sliders() {
         jQuery('.swiper-on-mobile-2').addClass('swiper swiper-on-mobile-2-js');
         jQuery('.swiper-on-mobile-2 > *').addClass('swiper-wrapper');
         jQuery('.swiper-on-mobile-2 > * > *').removeClass().addClass('swiper-slide');
-        jQuery('<div class="swiper-button-next swiper-button"></div><div class="swiper-button-prev swiper-button"></div>').appendTo('.swiper-on-mobile-2');
+        jQuery('<div class="swiper-button-next"></div><div class="swiper-button-prev"></div>').appendTo('.swiper-on-mobile-2');
         var swiper_on_mobile2 = new Swiper('.swiper-on-mobile-2-js', {
             slidesPerView: 1,
             navigation: {
