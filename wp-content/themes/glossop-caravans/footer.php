ec1f198430c57b920359d17188aab5b65ee2dbf0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-12 18:39:11 +0800

    Update footer.php

--- wp-content/themes/glossop-caravans/footer.php
+++ wp-content/themes/glossop-caravans/footer.php
@@ -22,7 +22,7 @@
     <?= do_shortcode('[template template_id=1602]') ?>
     <?= do_shortcode('[template template_id=1618]') ?>
     <?= do_shortcode('[template template_id=3897]') ?>
-    <?= do_shortcode('[template template_id=1719]') ?>
+    <?= do_shortcode('[template template_id=5358]') ?>
 
 </div>
 </div><!-- #page -->
