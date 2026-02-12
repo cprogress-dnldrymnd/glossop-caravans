548bf2d035adf404bd90f4aee6862d45efe702ee
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-12 18:37:18 +0800

    Update footer.php

--- wp-content/themes/glossop-caravans/footer.php
+++ wp-content/themes/glossop-caravans/footer.php
@@ -21,11 +21,15 @@
     <?= do_shortcode('[template template_id=1719]') ?>
     <?= do_shortcode('[template template_id=1602]') ?>
     <?= do_shortcode('[template template_id=1618]') ?>
-    <?= do_shortcode('[template template_id=1719]') ?>
+    <?= do_shortcode('[template template_id=3897]') ?>
 </div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<div class="container">
+<p style="font-size:16px;">Designed & Produced by <a href="https://digitallydisruptive.co.uk/">Digitally Disruptive</a></p>
+</div>
 
 <?php
 get_template_part('template-parts/offcanvas/finance-calculator');
